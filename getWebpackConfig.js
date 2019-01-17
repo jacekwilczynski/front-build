fc4d3a804d5a8a2b6506866d341d1f66adc76697
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2019-01-17 12:20:14 +0100

    Add Twig support

--- getWebpackConfig.js
+++ getWebpackConfig.js
@@ -85,6 +85,10 @@ module.exports = function getWebpackConfig({
           ]
         },
         {
+          test: /\.twig$/,
+          use: 'twig-loader'
+        },
+        {
           test: [
             /\.jpe?g$/,
             /\.png/,
